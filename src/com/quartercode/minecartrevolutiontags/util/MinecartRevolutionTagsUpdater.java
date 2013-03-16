bce352372f8e5453384dadb4d9118f04cfd817c0
LoadingByte <loadingbyte@gmail.com>
2013-03-16 09:22:26 +0100

    QuarterBukkit Beta 1.1.1 Update
    We updated the MinecartRevolution-code to the latest dev-version of
    QuarterBukkit.

--- src/com/quartercode/minecartrevolutiontags/util/MinecartRevolutionTagsUpdater.java
+++ src/com/quartercode/minecartrevolutiontags/util/MinecartRevolutionTagsUpdater.java
@@ -8,7 +8,7 @@ import org.bukkit.command.CommandSender;
 import com.quartercode.minecartrevolution.get.Lang;
 import com.quartercode.minecartrevolutiontags.MinecartRevolutionTags;
 import com.quartercode.quarterbukkit.api.Updater;
-import com.quartercode.quarterbukkit.api.exception.ExceptionManager;
+import com.quartercode.quarterbukkit.api.exception.ExceptionHandler;
 import com.quartercode.quarterbukkit.api.exception.InstallException;
 
 public class MinecartRevolutionTagsUpdater extends Updater {
@@ -33,7 +33,7 @@ public class MinecartRevolutionTagsUpdater extends Updater {
             return true;
         }
         catch (final Exception e) {
-            ExceptionManager.exception(new InstallException(plugin, this, e, Lang.getValue("basiccommands.update.error", "plugin", updatePlugin.getName(), "error", "Error while reloading: " + e.getLocalizedMessage())));
+            ExceptionHandler.exception(new InstallException(plugin, this, e, Lang.getValue("basiccommands.update.error", "plugin", updatePlugin.getName(), "error", "Error while reloading: " + e.getLocalizedMessage())));
         }
 
         return false;
--- src/com/quartercode/minecartrevolutiontags/util/TagManager.java
+++ src/com/quartercode/minecartrevolutiontags/util/TagManager.java
@@ -10,7 +10,7 @@ import com.quartercode.minecartrevolution.exception.MinecartRevolutionException;
 import com.quartercode.minecartrevolution.exception.MinecartRevolutionSilenceException;
 import com.quartercode.minecartrevolutiontags.MinecartRevolutionTags;
 import com.quartercode.qcutil.io.Properties;
-import com.quartercode.quarterbukkit.api.exception.ExceptionManager;
+import com.quartercode.quarterbukkit.api.exception.ExceptionHandler;
 
 public class TagManager {
 
@@ -28,7 +28,7 @@ public class TagManager {
                 tags.load(minecartRevolutionTags.getTagFile());
             }
             catch (final IOException e) {
-                ExceptionManager.exception(new MinecartRevolutionSilenceException(minecartRevolutionTags.getMinecartRevolution(), e, "Can't load tag file!"));
+                ExceptionHandler.exception(new MinecartRevolutionSilenceException(minecartRevolutionTags.getMinecartRevolution(), e, "Can't load tag file!"));
                 save();
             }
         } else {
@@ -96,7 +96,7 @@ public class TagManager {
             tags.store(minecartRevolutionTags.getTagFile(), "Tag Store File", "Do not edit!");
         }
         catch (final IOException e) {
-            ExceptionManager.exception(new MinecartRevolutionException(minecartRevolutionTags.getMinecartRevolution(), e, "Can't save tag file!"));
+            ExceptionHandler.exception(new MinecartRevolutionException(minecartRevolutionTags.getMinecartRevolution(), e, "Can't save tag file!"));
         }
     }
 
